ebf6b56b48cc10df152c851b8f78b8644088f14e
tongjilibo <tongjilibo@163.com>
2024-02-26 18:59:20 +0800

    同步代码

--- step1_pretrain.py
+++ step1_pretrain.py
@@ -1,32 +1,32 @@
 #! -*- coding: utf-8 -*-
 
-from bert4torch.models import build_transformer_model
-from bert4torch.snippets import sequence_padding
 import torch.nn as nn
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torch
-from bert4torch.models import build_transformer_model
-from bert4torch.snippets import IterDataset, DottableDict
-from bert4torch.callbacks import Callback, Logger, EarlyStopping, Tensorboard, Evaluator
+from bert4torch.models import build_transformer_model, BaseModel
+from bert4torch.snippets import IterDataset, DottableDict, log_info, get_weight_decay_optim_groups
+from bert4torch.callbacks import Checkpoint, Logger, EarlyStopping, Tensorboard, Evaluator
 from bert4torch.optimizers import get_linear_schedule_with_warmup
 from tqdm import tqdm
 from glob import glob
 import os
 import numpy as np
+import inspect
 
 
 # 基本参数
 args = DottableDict()
-args.lr = 5e-5
-args.batch_size = 8
+args.lr = 3e-4
+args.batch_size = 32
 args.eval_batch_size = 4
 args.grad_accumulation_steps = 1
 args.pad_token_id = 0
-args.max_length = 512
+args.max_length = 1024
 args.epochs = 1
-args.data_path = 'F:/data/pretrain_data_bin/**/*.bin'
+args.weight_decay = 0.1
+args.data_path = '/home/hfai/data/pretrain/pretrain_data_bin/**/*.bin'
 args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 args.dir_path = './config'
 args.config_path = os.path.join(args.dir_path, 'bert4torch_config.json')
@@ -34,36 +34,73 @@ args.config_path = os.path.join(args.dir_path, 'bert4torch_config.json')
 
 # ========================加载数据集========================
 filenames = glob(args.data_path, recursive=True)
-smp_size = 0
+filenames = [i for i in filenames if 'wudaocorpus' not in i]  # 除去wudao外的140亿tokens
+token_size, smp_size = 0, 0
 for filename in filenames:
     with open(filename,'r') as f:
         nbytes = f.seek(0,2)
         flen = f.tell() // np.dtype('uint16').itemsize
+    token_size += flen
     smp_size += flen//args.max_length
 args.steps_per_epoch = smp_size // (args.batch_size * args.grad_accumulation_steps)
+log_info(f'token_size: {token_size}, smp_size: {smp_size}, steps_per_epoch: {args.steps_per_epoch}')
 
 class MyDataset(IterDataset):
-    @staticmethod
-    def load_data(filenames):
+    def __init__(self, file_path=None, memmap=False, **kwargs):
+        self.memmap = memmap
+        super().__init__(file_path, **kwargs)
+
+    def load_data(self, filenames):
         """加载数据，并尽量分为不超过maxlen的句子
         """
         for filename in filenames:
-            with open(filename,'r') as f:
-                nbytes = f.seek(0,2)
-                flen = f.tell() // np.dtype('uint16').itemsize
-            data = np.memmap(filename, dtype=np.dtype('uint16'),shape=(flen//args.max_length, args.max_length))
+            if self.memmap:
+                with open(filename,'r') as f:
+                    nbytes = f.seek(0,2)
+                    flen = f.tell() // np.dtype('uint16').itemsize
+                data = np.memmap(filename, dtype=np.dtype('uint16'),shape=(flen//args.max_length, args.max_length))
+            else:
+                with open(filename,'r') as f:
+                    data=np.fromfile(f,dtype=np.uint16)
+                data = data[:args.max_length*int(len(data)/args.max_length)]
+                data = data.reshape(-1, args.max_length)
+                data = torch.from_numpy(np.array(data).astype(np.int64))
             for item in data:
-                yield item
+                yield item[..., :-1], item[..., 1:]
 
-def collate_fn(batch_token_ids):
-    batch_token_ids = torch.from_numpy(sequence_padding(batch_token_ids, value=args.pad_token_id).astype(np.int64))
-    return [batch_token_ids[..., :-1]], batch_token_ids[..., 1:]
-
-train_dataloader = DataLoader(MyDataset(filenames), batch_size=args.batch_size, collate_fn=collate_fn) 
-
+train_dataloader = DataLoader(MyDataset(filenames), batch_size=args.batch_size, pin_memory=False, 
+                              drop_last=False, shuffle=False, num_workers=0 if os.name == 'nt' else 4) 
 
 # ========================建立模型========================
+def init_model():
+    # model init
+    # model init
+    model_args = dict(
+        dim=1024,
+        n_layers=12,
+        n_heads=8,
+        n_kv_heads=8,
+        vocab_size=64793,
+        multiple_of=32,
+        max_seq_len=1024,
+        dropout=0,
+    )  # start with model_args from command line
+        # init a new model from scratch
+    print("Initializing a new model from scratch")
+    from model import Transformer, ModelArgs
+    gptconf = ModelArgs(**model_args)
+    model = Transformer(gptconf)
+    return model
+
+# class Model(BaseModel):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.module = init_model()
+#     def forward(self, *inputs, **input_kwargs):
+#         return self.module.forward(*inputs, **input_kwargs)
+# model = Model().to(args.device)
 model = build_transformer_model(config_path=args.config_path, checkpoint_path=None, add_trainer=True).to(args.device)
+model.print_trainable_parameters()
 
 class CrossEntropyLoss(nn.CrossEntropyLoss):
     def __init__(self, **kwargs):
@@ -81,17 +118,21 @@ class CrossEntropyLoss(nn.CrossEntropyLoss):
 
         return loss.to(raw_dtyps)
 
-optimizer = optim.AdamW(model.parameters(), args.lr)
-scheduler = get_linear_schedule_with_warmup(optimizer, 0, args.steps_per_epoch*args.epochs)
-model.compile(loss=CrossEntropyLoss(ignore_index=args.pad_token_id), optimizer=optimizer, scheduler=scheduler, 
-              grad_accumulation_steps=args.grad_accumulation_steps, clip_grad_norm=1.0)
+# 创建optimizer
+optim_groups = get_weight_decay_optim_groups(model, weight_decay=args.weight_decay)
+use_fused = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+extra_args = dict(fused=True) if use_fused else dict()
+optimizer = optim.AdamW(optim_groups, lr=args.lr, betas=(0.9, 0.95), **extra_args)
 
+scheduler = get_linear_schedule_with_warmup(optimizer, 5000, args.steps_per_epoch*args.epochs)
+model.compile(loss=CrossEntropyLoss(ignore_index=args.pad_token_id), optimizer=optimizer, scheduler=scheduler, 
+              grad_accumulation_steps=args.grad_accumulation_steps, clip_grad_norm=1.0, mixed_precision=True)
 
 
 if __name__ == '__main__':
     logger = Logger('./ckpt/log_pretrain.log')
-    evaluator = Evaluator(monitor='loss', method='step', mode='min', verbose=0, save_dir='./ckpt/best')
-    early_stop = EarlyStopping(monitor='loss', verbose=1)
+    evaluator = Checkpoint(monitor='loss', method='step', mode='min', verbose=0, interval=2000, save_dir='./ckpt/{step}', max_save_count=3)
+    early_stop = EarlyStopping(monitor='loss', verbose=1, patience=3, interval=2000)
     ts_board = Tensorboard('./ckpt/tensorboard')  # tensorboard
 
     model.fit(train_dataloader, steps_per_epoch=args.steps_per_epoch, epochs=args.epochs, 
