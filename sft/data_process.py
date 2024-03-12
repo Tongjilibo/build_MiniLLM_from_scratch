3001d0afb11bf584de713575f7a5ef42abf6a860
unknown <H01305@HAFM.LOCAL>
2024-03-12 15:34:52 +0800

    增加sft代码

---
+++ sft/data_process.py
@@ -0,0 +1,266 @@
+''' 数据处理模块
+1. 增加HUMAN和ROBOT标记，可以用于多轮对话问答
+2. 不限制prompt和answer的长度，仅限制总长度，可容纳更多的样本
+3. 多轮对话中，同时计算多个answer的loss, 提升训练效率
+'''
+import json
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+import torch
+from bert4torch.snippets import sequence_padding
+import re
+from tqdm import tqdm
+
+MAX_LENGTH = 1024
+HUMAN = '<human>'
+ROBOT = '<robot>'
+PAD_TOKEN_ID = 0
+EOS_TOKEN_ID = 2
+MAX_SAMPLES = 1000  # None表示不限制，不为None用于测试小样本快速验证
+
+
+def process_alpaca(data_path, tokenizer):
+    '''alpaca_gpt4_data_zh.json'''
+    with open(data_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    res = []
+    for per in data:
+        q = tokenizer.encode(HUMAN + per['instruction'] + per['input'] + ROBOT, add_special_tokens=False)
+        a = tokenizer.encode(per['output'], add_special_tokens=False)
+        if len(q) + len(a) >= MAX_LENGTH:
+            continue
+        input_ids = q + a
+        labels = [PAD_TOKEN_ID] * (len(q)-1) + input_ids[len(q):] + [EOS_TOKEN_ID]
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_belle(data_path, tokenizer):
+    '''Belle_open_source_1M.json'''
+    f = open(data_path, 'r', encoding='utf-8')
+    
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per = json.loads(line)
+        q = tokenizer.encode(HUMAN + per['instruction'] + per['input'] + ROBOT, add_special_tokens=False)
+        a = tokenizer.encode(per['output'], add_special_tokens=False)
+        if len(q) + len(a) >= MAX_LENGTH:
+            continue
+        input_ids = q + a
+        labels = [PAD_TOKEN_ID] * (len(q)-1) + input_ids[len(q):] + [EOS_TOKEN_ID]
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_deepctrl(data_path, tokenizer):
+    '''deepctrl-sft-data'''
+    f = open(data_path, 'r', encoding='utf-8')
+    
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per = json.loads(line)
+
+        input_ids, labels = [], []
+        for human, robot in per['history']:
+            human = tokenizer.encode(HUMAN + human + ROBOT, add_special_tokens=False)
+            robot = tokenizer.encode(robot, add_special_tokens=False)
+            # 轮次太多的话，则进行截断
+            if len(human + robot) >= MAX_LENGTH:
+                break
+            input_ids.extend(human + robot)
+            labels.extend([PAD_TOKEN_ID]*(len(human)-1) + robot + [EOS_TOKEN_ID])
+            
+        q = tokenizer.encode(HUMAN + per['instruction'] + per['input'] + ROBOT, add_special_tokens=False)
+        input_ids.extend(q)
+        a = tokenizer.encode(per['output'], add_special_tokens=False)
+        labels.extend([PAD_TOKEN_ID]*(len(q)-1) + a)
+        if len(input_ids) >= MAX_LENGTH:
+            continue
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_moss002(data_path, tokenizer):
+    '''fnlp@moss-002-sft-data'''
+    with open(data_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    res = []
+    for per in data:
+        history = re.split('<eoh> \[MOSS\]: |<eoa> \[Human\]: |\[Human\]: |<eoa>', per['plain_text'])
+        history = [i.strip() for i in history if i]
+        input_ids, labels = [], []
+        for human, robot in zip(history[0::2], history[1::2]):
+            human = tokenizer.encode(HUMAN + human + ROBOT, add_special_tokens=False)
+            robot = tokenizer.encode(robot, add_special_tokens=False)
+            # 轮次太多的话，则进行截断
+            if len(human + robot) >= MAX_LENGTH:
+                break
+            input_ids.extend(human + robot)
+            labels.extend([PAD_TOKEN_ID]*(len(human)-1) + robot + [EOS_TOKEN_ID])
+
+        if len(input_ids) >= MAX_LENGTH:
+            continue
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_moss003(data_path, tokenizer):
+    '''fnlp@moss-003-sft-data'''
+    f = open(data_path, 'r', encoding='utf-8')
+    
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per = json.loads(line)
+        input_ids, labels = [], []
+        for turn in per['chat'].values():
+            if not re.search(r'[\u4e00-\u9fff]', turn['MOSS']):
+                continue
+
+            human = turn['Human'].replace('<|Human|>: ', '').replace('<eoh>\n', '')
+            robot = turn['MOSS'].replace('<|MOSS|>: ', '').replace('<eom>\n', '')
+            robot = re.sub('<sup><\|[0-9]+\|></sup>', '', robot).strip()
+
+            human = tokenizer.encode(HUMAN + human + ROBOT, add_special_tokens=False)
+            robot = tokenizer.encode(robot, add_special_tokens=False)
+            # 轮次太多的话，则进行截断
+            if len(human + robot) >= MAX_LENGTH:
+                break
+            input_ids.extend(human + robot)
+            labels.extend([PAD_TOKEN_ID]*(len(human)-1) + robot + [EOS_TOKEN_ID])
+
+        if len(input_ids) >= MAX_LENGTH:
+            continue
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_shareai(data_path, tokenizer):
+    '''shareAI'''
+    f = open(data_path, 'r', encoding='utf-8')
+    
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per = json.loads(line)
+        input_ids, labels = [], []
+        for turn in per['conversation']:
+            human = turn['human']
+            robot = turn['assistant']
+
+            human = tokenizer.encode(HUMAN + human + ROBOT, add_special_tokens=False)
+            robot = tokenizer.encode(robot, add_special_tokens=False)
+            # 轮次太多的话，则进行截断
+            if len(human + robot) >= MAX_LENGTH:
+                break
+            input_ids.extend(human + robot)
+            labels.extend([PAD_TOKEN_ID]*(len(human)-1) + robot + [EOS_TOKEN_ID])
+
+        if len(input_ids) >= MAX_LENGTH:
+            continue
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+def process_firefly(data_path, tokenizer):
+    '''YeungNLP@firefly-train-1.1M'''
+    f = open(data_path, 'r', encoding='utf-8')
+    
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per = json.loads(line)
+        q = tokenizer.encode(HUMAN + per['input'] + ROBOT, add_special_tokens=False)
+        a = tokenizer.encode(per['target'], add_special_tokens=False)
+        if len(q) + len(a) >= MAX_LENGTH:
+            continue
+        input_ids = q + a
+        labels = [PAD_TOKEN_ID] * (len(q)-1) + input_ids[len(q):] + [EOS_TOKEN_ID]
+        res.append((input_ids, labels))
+        if (MAX_LENGTH is not None) and (len(res) >= MAX_SAMPLES):
+            break
+    return res
+
+
+MAPPING = {
+    'alpaca-zh/alpaca_gpt4_data_zh.json': process_alpaca,
+    'train_1M_CN/Belle_open_source_1M.json': process_belle,
+    'train_0.5M_CN/Belle_open_source_0.5M.json': process_belle,
+    'school_math_0.25M/school_math_0.25M.json': process_belle,
+    'deepctrl-sft-data/sft_data_zh.jsonl': process_deepctrl,
+    'moss-002-sft-data/zh_helpfulness.json': process_moss002,
+    'moss-002-sft-data/zh_honesty.json': process_moss002,
+    'moss-003-sft-data/conversations_with_tools_with_inner_instruction_no_text2image_train_all_random_meta0.5_0.1_0.01_moss_0709.jsonl': process_moss003,
+    'moss-003-sft-data/moss-003-sft-no-tools.jsonl': process_moss003,
+    'CodeChat/continue_zh.jsonl': process_shareai,
+    'CodeChat/continue_zh_2.jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/common_zh_70k.jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/computer_cn_26k_continue.jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/computer_en_26k(fixed).jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/computer_zh_26k(fixed).jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/computer_zh_26k.jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/unknow_zh_38k.jsonl': process_shareai,
+    'ShareGPT-Chinese-English-90k/unknow_zh_38k_continue.jsonl': process_shareai,
+    'firefly-train-1.1M/firefly-train-1.1M.jsonl': process_firefly
+}
+
+class SFTDataset(Dataset):
+    def __init__(self, filename, tokenizer):
+        super().__init__()
+        self.MAX_LENGTH = MAX_LENGTH
+        self.tokenizer = tokenizer
+        self.data = self.load_data(filename)
+    
+    def load_data(self, filename):
+        postfix = filename.split('@')[-1]
+        return MAPPING[postfix](filename, self.tokenizer)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index: int):
+        return self.data[index]
+
+def collate_train_fn(batch):
+    batch_token_ids = [i[0] for i in batch]
+    batch_labels = [i[1] for i in batch]
+    batch_token_ids = torch.tensor(sequence_padding(batch_token_ids, value=PAD_TOKEN_ID), dtype=torch.long)
+    batch_labels = torch.tensor(sequence_padding(batch_labels, value=PAD_TOKEN_ID), dtype=torch.long)
+    return [batch_token_ids], batch_labels
+
+
+if __name__ == '__main__':
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained('../config', trust_remote_code=True)
+    # process_moss002('F:/data/corpus/sft/common/fnlp@moss-002-sft-data/zh_helpfulness.json', tokenizer)
+    # process_moss003_with_tools('F:/data/corpus/sft/common/fnlp@moss-003-sft-data/conversations_with_tools_with_inner_instruction_no_text2image_train_all_random_meta0.5_0.1_0.01_moss_0709.jsonl', tokenizer)
+    # process_moss003('F:/data/corpus/sft/common/fnlp@moss-003-sft-data/moss-003-sft-no-tools.jsonl', tokenizer)
+    # process_codechat('F:\data\corpus\sft\common\shareAI@CodeChat/continue_zh_2.jsonl', tokenizer)
+    # process_sharegpt('F:\data\corpus\sft\common\shareAI@ShareGPT-Chinese-English-90k\common_zh_70k.jsonl', tokenizer)
+    process_firefly('F:\data\corpus\sft\common\YeungNLP@firefly-train-1.1M/firefly-train-1.1M.jsonl', tokenizer)
---
+++ sft/sft.py
@@ -0,0 +1,122 @@
+#! -*- coding: utf-8 -*-
+'''
+指令微调
+启动命令: nohup torchrun --standalone --nproc_per_node=4 pretrain.py --name baby > nohup.log&
+'''
+import torch.nn as nn
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from data_process import SFTDataset, collate_train_fn, PAD_TOKEN_ID
+from torch.utils.data.distributed import DistributedSampler
+from bert4torch.models import build_transformer_model, BaseModel, BaseModelDDP
+from bert4torch.snippets import ListDataset, DottableDict, log_info, get_weight_decay_optim_groups
+from bert4torch.callbacks import Callback, Checkpoint, Logger, EarlyStopping, Tensorboard, Evaluator
+from bert4torch.optimizers import get_linear_schedule_with_warmup
+from collections import deque
+import os
+import numpy as np
+import inspect
+from transformers import AutoTokenizer
+
+
+# 基本参数
+args = DottableDict()
+args.ddp_config = BaseModelDDP.init_process_group() if int(os.environ.get("RANK", -1)) != -1 else None
+args.lr = 2e-5
+args.batch_size = 8
+args.grad_accumulation_steps = 1
+args.max_length = 1024
+args.epochs = 1
+args.weight_decay = 0.1
+args.interval = 2000
+args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+args.config_path = '../config'
+args.model_path = '../ckpt/L12_H1024_A8-Wudao/final/model.pt'
+args.save_dir = '../ckpt/L12_H1024_A8-Wudao-SFT'
+args.filenames = [
+    'shibing624@alpaca-zh/alpaca_gpt4_data_zh.json',
+    'BelleGroup@train_0.5M_CN/Belle_open_source_0.5M'
+    'BelleGroup@train_1M_CN/Belle_open_source_1M.json',
+    'BelleGroup@school_math_0.25M/school_math_0.25M.json',
+    'deepctrl@deepctrl-sft-data/sft_data_zh.jsonl',
+    'fnlp@moss-002-sft-data/zh_helpfulness.json',
+    'fnlp@moss-002-sft-data/zh_honesty.json',
+    'fnlp@moss-003-sft-data/conversations_with_tools_with_inner_instruction_no_text2image_train_all_random_meta0.5_0.1_0.01_moss_0709.jsonl',
+    'fnlp@moss-003-sft-data/moss-003-sft-no-tools.jsonl',
+    'shareAI@CodeChat/continue_zh.jsonl',
+    'shareAI@CodeChat/continue_zh_2.jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/common_zh_70k.jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/computer_cn_26k_continue.jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/computer_en_26k(fixed).jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/computer_zh_26k(fixed).jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/computer_zh_26k.jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/unknow_zh_38k.jsonl',
+    'shareAI@ShareGPT-Chinese-English-90k/unknow_zh_38k_continue.jsonl',
+    'YeungNLP@firefly-train-1.1M/firefly-train-1.1M.jsonl'
+    ]
+args.filenames = deque(['/data/corpus/sft/common/' + i for i in args.filenames])
+
+# ========================加载数据集========================
+tokenizer = AutoTokenizer.from_pretrained(args.config_path, trust_remote_code=True)
+def get_trainloader(args):
+    filename = args.filenames.popleft()
+    dataset = SFTDataset(filename, tokenizer)
+    train_dataloader = DataLoader(dataset, batch_size=args.batch_size, pin_memory=False, 
+                                drop_last=False, shuffle=False, num_workers=0 if os.name == 'nt' else 2,
+                                sampler=DistributedSampler(dataset) if args.ddp_config is not None else None,
+                                collate_fn=collate_train_fn)
+    return train_dataloader
+
+train_dataloader = get_trainloader(args)
+model = build_transformer_model(config_path=args.config_path, checkpoint_path=None, add_trainer=True)
+model.to(args.device)
+model.load_weights(args.model_path, mapping=lambda x: x.replace('module.', ''))  # 加载预训练权重
+
+if args.ddp_config is not None:
+    model = BaseModelDDP(model, master_rank=0, device_ids=[args.ddp_config.local_rank], output_device=args.ddp_config.local_rank, find_unused_parameters=False)
+model.print_trainable_parameters()
+
+class CrossEntropyLoss(nn.CrossEntropyLoss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def forward(self, logits, labels):
+        '''
+        logits: [btz, seq_len, vocab_size]
+        labels: token_ids: [btz, seq_len]
+        '''
+        raw_dtyps = logits.dtype
+        logits = logits.to(torch.float32)        
+        logits = logits.reshape(-1, logits.shape[-1])
+        labels = labels.flatten()
+        loss = super().forward(logits, labels)
+
+        return loss.to(raw_dtyps)
+
+# 创建optimizer
+optim_groups = get_weight_decay_optim_groups(model, weight_decay=args.weight_decay)
+use_fused = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+extra_args = dict(fused=True) if use_fused else dict()
+optimizer = optim.AdamW(optim_groups, lr=args.lr, betas=(0.9, 0.95), **extra_args)
+
+model.compile(loss=CrossEntropyLoss(ignore_index=PAD_TOKEN_ID), optimizer=optimizer, 
+              grad_accumulation_steps=args.grad_accumulation_steps, clip_grad_norm=1.0, mixed_precision=True)
+
+class GenTrainLoader(Callback):
+    """自动保存最新模型
+    """
+    def on_dataloader_end(self, logs=None):
+        model.train_dataloader = get_trainloader(args)
+
+
+if __name__ == '__main__':
+    logger = Logger(args.save_dir+'/log_sft.log')
+    checkpoint = Checkpoint(monitor='loss', epoch_or_step='step', min_max='min', verbose=0, interval=args.interval, 
+                            save_dir=args.save_dir+'/{step}_{loss:.4f}', max_save_count=5, save_on_train_end=True)
+    early_stop = EarlyStopping(monitor='loss', verbose=1, patience=3*args.interval)
+    ts_board = Tensorboard(args.save_dir+'/tensorboard')  # tensorboard
+    callbacks=[checkpoint, logger, ts_board]
+    if args.ddp_config is not None:
+        model.disable_run_callbacks(callbacks)
+
+    model.fit(train_dataloader, steps_per_epoch=None, epochs=args.epochs, callbacks=[GenTrainLoader()]+callbacks)
